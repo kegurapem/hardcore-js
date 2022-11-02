576e88f192a870e0559e38569b87de5a39d6e930
kevin gutierrez <harold.dev1993@gmail.com>
2022-11-02 01:35:47 -0500

    avanzando objetos ando

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log('My first code')
+const gato = {
+    nombre: 'Valiente',
+    duerme: true,
+    edad: 10,
+    enemigos: ["agua", "perros"]
+}
+
+//---------------------------------------------------------------
+// ACCEDER A UN OBJETO
+
+//Notación de punto
+console.log(gato.nombre)
+console.log(gato.duerme)
+console.log(gato.enemigos[0]);
+
+// Notación de corchetes
+console.log(gato['nombre'])
+console.log(gato['edad'])
+console.log(gato["enemigos"][0]);
+
+//---------------------------------------------------------------
+// CRUD
+
+// Create
+gato.color = 'White'
+gato['vidas'] = 7
+
+// Read
+console.log(gato)
+
+//Update
+gato.color = 'Black'
+gato['vidas'] = 6
+
+// Delete 
+delete gato.color
+delete gato['vidas']
+
+
+//---------------------------------------------------------------
+//hasOwnProperty comprueba si una propiedad existe: retorna true o false
+const gato = {
+    nombre: 'Valiente',
+    duerme: true,
+    edad: 10,
+    enemigos: ["agua", "perros"]
+}
+
+console.log(gato.hasOwnProperty("nombre")) // output true
+console.log(gato.hasOwnProperty("salud")) // output false
